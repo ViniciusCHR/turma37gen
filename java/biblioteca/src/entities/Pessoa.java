49c1700ad8c30cba8ed8a342b8737fca1b26c656
Vinicius Silva <vinicius.unicap@gmail.com>
2021-10-07 22:12:38 -0300

    listaClasse

---
+++ java/biblioteca/src/entities/Pessoa.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Pessoa {
+	public String nome;
+	public int anoNascimento;
+	public boolean viva;
+	public String email;
+	public char pronome;
+
+	public int calcularIdade() {
+		return 2021 - anoNascimento;
+	}
+	
+	public int calcularIdade(int ano) {
+		return ano - anoNascimento;
+	}
+	public String toString() {
+		return "Pessoa [nome=" + nome + ", anoNascimento=" + anoNascimento + "]";
+	}
+}
